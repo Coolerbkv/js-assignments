465b3016dd2571fb1a66d7e2089a2ca947895713
coolerbkv <coolerbkv@gmail.com>
2016-03-22 14:54:53 +0300

    Revert "Date tasks have been added"
    This reverts commit ddbc22c83bd760c2ea17d6d7e40e9bba64e774b3.

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -22,7 +22,7 @@
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
 function parseDataFromRfc2822(value) {
-   return  new Date(value);
+   throw new Error('Not implemented');
 }
 
 /**
@@ -37,7 +37,7 @@ function parseDataFromRfc2822(value) {
  *    '2016-01-19T08:07:37Z' => Date()
  */
 function parseDataFromIso8601(value) {
-   return Date.parse(value);
+   throw new Error('Not implemented');
 }
 
 
@@ -56,8 +56,7 @@ function parseDataFromIso8601(value) {
  *    Date(2015,1,1)    => false
  */
 function isLeapYear(date) {
-   return ((date.getFullYear() % 4 == 0) && (date.getFullYear() % 100 != 0)) 
-      || (date.getFullYear() % 400 == 0);
+   throw new Error('Not implemented');
 }
 
 
@@ -77,7 +76,7 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
 function timeSpanToString(startDate, endDate) {
-   return new Date(endDate - startDate).toISOString().slice(11, -1);
+   throw new Error('Not implemented');
 }
 
 
@@ -95,10 +94,7 @@ function timeSpanToString(startDate, endDate) {
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
 function angleBetweenClockHands(date) {
-   var minute = date.getUTCMinutes() * 11;
-   var hour = date.getUTCHours() % 12 * 60;
-   let angleClock = Math.abs(hour - minute) * 0.5 * Math.PI / 180;
-       return angleClock > Math.PI ? angleClock - Math.PI : angleClock;
+    throw new Error('Not implemented');
 }
 
 
